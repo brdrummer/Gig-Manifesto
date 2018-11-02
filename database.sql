9e113f6cea873d708aeeedd95f44faefa9730222
brdrummer <brdrummer@hotmail.com>
2018-11-02 09:29:38 -0500

    Present 2

--- database.sql
+++ database.sql
@@ -1,20 +1,7 @@
-
-
-CREATE TABLE "gigs" (
-    "id" SERIAL PRIMARY KEY,
-    "name" VARCHAR(120),
-    "startTime" VARCHAR(120),
-    "soundCheck" VARCHAR(120),
-    "address" VARCHAR(240),
-    "entryFee" INTEGER,
-    "image_url" VARCHAR (2083),
-    "user_gig_id" INT REFERENCES person
-);
-
 CREATE TABLE person (
     id SERIAL PRIMARY KEY,
     username VARCHAR (80) UNIQUE NOT NULL,
-    password VARCHAR (1000) NOT NULL,
+    password VARCHAR (1000) NOT NULL
 );
 
 CREATE TABLE "profile" (
@@ -22,8 +9,19 @@ CREATE TABLE "profile" (
     name VARCHAR (120),
     email VARCHAR(120),
     bands VARCHAR (1000),
-    city VARCHAR (120)
+    city VARCHAR (120),
+    image_url VARCHAR (2083),
     user_profile_id INT REFERENCES person
 );
-INSERT INTO "gigs" ("name", "startTime", "soundCheck", "address", "entryFee")
-VALUES ( 'Icehouse', '7:00', '6:00', '172 6th St, East', '7' );
+
+CREATE TABLE "gigs" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(120),
+    "date" VARCHAR(120),
+    "startTime" VARCHAR(120),
+    "soundCheck" VARCHAR(120),
+    "address" VARCHAR(240),
+    "entryFee" INTEGER,
+    "image_url" VARCHAR (2083),
+    "user_gig_id" INT REFERENCES person
+);
